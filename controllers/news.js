04deec9890971645d6ef0b5fcd50c4474de2f4c1
Norman Teodoro <normanteodoro@hotmail.com>
2022-02-28 16:50:16 -0800

    add news page

--- controllers/news.js
+++ controllers/news.js
@@ -18,6 +18,17 @@ router.get('/', async (req, res) => {
         res.render('news/news.ejs', {message: err})
     }
 })
+router.get('/:id', async (req, res) => {
+    let categeory = req.params.id
+    try  {
+        let endpoint = `https://finnhub.io/api/v1/news?category=${category}&token=${process.env.API_TOKEN}&minId=10`
+        const respNews = await axios.get(endpoint)
+        res.render('news/news.ejs',{message: null, news: respNews.data})
+
+    } catch(err) {
+        res.render('news/news.ejs', {message: err})
+    }
+})
 
 
 module.exports = router
